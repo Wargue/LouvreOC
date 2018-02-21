c58be429ace55d61cd28745bdf64a2817d8e1b28
Ouargue <gillet_l@hotmail.com>
2018-02-21 21:27:31 +0100

    Première phase visuelle

---
+++ src/LG/SaleBundle/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+// src/LG/SaleBundle/Controller/HomeController.php
+
+namespace LG\SaleBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HomeController extends Controller
+{
+
+    /**
+     * @route("/", name="home")
+     */
+    public function indexAction()
+    {
+        return $this->render('LGSaleBundle:Sale:index.html.twig');
+
+    }
+
+
+}
---
+++ src/LG/SaleBundle/LGSaleBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LG\SaleBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LGSaleBundle extends Bundle
+{
+}

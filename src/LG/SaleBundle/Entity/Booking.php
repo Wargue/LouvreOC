6fbc4fb57f2baa7b3a0468357c250686229d4525
Ouargue <gillet_l@hotmail.com>
2018-03-03 16:12:57 +0100

    Booking entity Creation

---
+++ src/LG/SaleBundle/Entity/Booking.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace LG\SaleBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Booking
+ *
+ * @ORM\Table(name="booking")
+ * @ORM\Entity(repositoryClass="LG\SaleBundle\Repository\BookingRepository")
+ */
+class Booking
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="visitDate", type="datetime")
+     */
+    private $visitDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contactMail", type="string", length=255)
+     */
+    private $contactMail;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="ticketNumber", type="integer")
+     */
+    private $ticketNumber;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="totalPrice", type="integer")
+     */
+    private $totalPrice;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set visitDate
+     *
+     * @param \DateTime $visitDate
+     *
+     * @return Booking
+     */
+    public function setVisitDate($visitDate)
+    {
+        $this->visitDate = $visitDate;
+
+        return $this;
+    }
+
+    /**
+     * Get visitDate
+     *
+     * @return \DateTime
+     */
+    public function getVisitDate()
+    {
+        return $this->visitDate;
+    }
+
+    /**
+     * Set contactMail
+     *
+     * @param string $contactMail
+     *
+     * @return Booking
+     */
+    public function setContactMail($contactMail)
+    {
+        $this->contactMail = $contactMail;
+
+        return $this;
+    }
+
+    /**
+     * Get contactMail
+     *
+     * @return string
+     */
+    public function getContactMail()
+    {
+        return $this->contactMail;
+    }
+
+    /**
+     * Set ticketNumber
+     *
+     * @param integer $ticketNumber
+     *
+     * @return Booking
+     */
+    public function setTicketNumber($ticketNumber)
+    {
+        $this->ticketNumber = $ticketNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get ticketNumber
+     *
+     * @return int
+     */
+    public function getTicketNumber()
+    {
+        return $this->ticketNumber;
+    }
+
+    /**
+     * Set totalPrice
+     *
+     * @param integer $totalPrice
+     *
+     * @return Booking
+     */
+    public function setTotalPrice($totalPrice)
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get totalPrice
+     *
+     * @return int
+     */
+    public function getTotalPrice()
+    {
+        return $this->totalPrice;
+    }
+}
+
